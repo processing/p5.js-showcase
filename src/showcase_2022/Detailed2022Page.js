44918d8f5ceb911d2d6bb5a86f50e00ca63568c1
Annie Zheng <anniezhe@usc.edu>
2022-10-26 14:08:46 -0700

    card changes + translation updates

--- src/showcase_2022/Detailed2022Page.js
+++ src/showcase_2022/Detailed2022Page.js
@@ -1,20 +1,15 @@
 import "../styles/App.css";
 import React from "react";
-import Details2022 from "./Details2022.js"
-import i18next from 'i18next';
+import Details2022 from "./Details2022.js";
+import i18next from "i18next";
 import { useParams } from "react-router-dom";
 
 export default function Detailed2022Page() {
-    let { id, filter } = useParams();
-    let entries = i18next.t('showcase2020', { returnObjects: true });
-    let piece = entries.find(x => x.id === id);
-  
-    if (!piece) return <div>Image not found</div>;
-  
-    return (
-      <Details2022
-        filter={filter}
-        piece={piece}
-      />
-    );
-  }
+  let { id, filter } = useParams();
+  let entries = i18next.t("showcase2022", { returnObjects: true });
+  let piece = entries.find((x) => x.id === id);
+
+  if (!piece) return <div>Image not found</div>;
+
+  return <Details2022 filter={filter} piece={piece} />;
+}
